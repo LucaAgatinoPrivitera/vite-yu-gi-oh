deaaac3f94fd57b6a1b8d536df94093cdd87be78
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-27 15:43:05 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="d-flex mx-4 my-2">
+    <img src="" alt="">
+    <h1>Yu-Gi-Oh-Api</h1>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <div class="d-flex px-4 py-4 w-100">
+    <div class="w-75 m-auto">
+      <button class="btn btn-outline-secondary dropdown-toggle bg-white" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Alien</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: orange;
+}
+button{
+  padding-inline-end: 2rem;
+}
+</style>
