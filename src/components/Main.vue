152e33157a9339a111224005e8c18d9a20c28cd7
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-27 15:48:34 +0200

    Ho messo l'export default comp al main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,31 @@
 <script>
 import { ref } from 'vue'
+import Main_ContainerCards from '../components/Main_ContainerCards.vue'
+
+
+export default {
+  components: {
+    Main_ContainerCards
+  },
+}
 </script>
 
 <template>
   <div class="d-flex px-4 py-4 w-100">
     <div class="w-75 m-auto">
-      <button class="btn btn-outline-secondary dropdown-toggle bg-white" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Alien</button>
+      <button class="btn btn-outline-secondary dropdown-toggle bg-white" type="button" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">Alien</button>
     </div>
   </div>
+  <Main_ContainerCards></Main_ContainerCards>
 </template>
 
 <style scoped>
 div {
   background-color: orange;
 }
-button{
+
+button {
   padding-inline-end: 2rem;
 }
 </style>
---
+++ src/components/Main_ContainerCards.vue
@@ -0,0 +1,22 @@
+<script>
+import { ref } from 'vue'
+</script>
+
+<template>
+    <div class="d-flex px-4 py-4 w-100">
+        <div>
+            <h3>Found 39 cards</h3>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+div {
+    background-color: orange;
+}
+
+button {
+    padding-inline-end: 2rem;
+}
+</style>
